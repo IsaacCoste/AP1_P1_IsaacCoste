6d27b638ca3da091956a4cddec40b7fe4f9056b1
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-06-03 18:34:59 -0400

    Modelo de Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP1_P1_IsaacCoste.Models;
+//articuloId, Descripcion, Costo, Ganancia y precio
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion { get; set; }
+    [Range(0.01, float.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
+    [Required(ErrorMessage ="Este campo es obligatorio.")]
+    public decimal Costo { get; set; }
+    [Range(0.01, float.MaxValue, ErrorMessage = "La ganancia debe ser mayor a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public float? Ganancia { get; set; }
+    [Range(0.01, float.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Precio { get; set; }
+}
