ea1f1acef4ecb5dc80689372bbae120564aeeb78
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-06-03 18:54:19 -0400

    agrege el modelo al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AP1_P1_IsaacCoste.Models;
+using Microsoft.EntityFrameworkCore;
 namespace AP1_P1_IsaacCoste.DAL;
 public class Contexto : DbContext
 { 
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Articulos> Articulos { get; set; }
 }
